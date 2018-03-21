23619e45faf0cc4fcf45b11b4be0a5dc19c11211
MigueGo <lmsg@live.com>
2018-03-21 10:12:31 +0100

    trim space in server's name
    $server = $server.trim()

--- Get-SPFarmLogs.ps1
+++ Get-SPFarmLogs.ps1
@@ -328,6 +328,7 @@ foreach($server in $srvs){
 	Write-Host("-------//-------") -ForegroundColor Magenta;
     write-host("Processing the server: $server") -ForegroundColor Magenta ;
     #check if server is available to PING or fileshare access
+	$server = $server.trim()
     if(!((Test-Connection -Quiet $server -Count 2) -or (Test-Path "\\$server\c$"))) {
 		write-host("[$server] connection or server not available") -ForegroundColor Red;
     }
